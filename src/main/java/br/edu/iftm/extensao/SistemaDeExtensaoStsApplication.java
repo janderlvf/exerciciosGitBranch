a17a77122893aa8d795f8948de5ba131364253ff
Jander Viana Fialho <jvianafi@everis.com>
2019-02-26 20:25:09 -0300

    versao inicial do projeto

---
+++ src/main/java/br/edu/iftm/extensao/SistemaDeExtensaoStsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.extensao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeExtensaoStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeExtensaoStsApplication.class, args);
+	}
+
+}
